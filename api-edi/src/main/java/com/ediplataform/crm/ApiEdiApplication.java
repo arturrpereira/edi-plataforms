3e292c8faa7037eadd85714c7f7364edde838882
arturrpereira <artur.fernando@protonmail.com>
2024-01-13 12:35:31 -0300

    First Commit

---
+++ api-edi/src/main/java/com/ediplataform/crm/ApiEdiApplication.java
@@ -0,0 +1,13 @@
+package com.ediplataform.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEdiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEdiApplication.class, args);
+	}
+
+}
---
+++ api-edi/src/test/java/com/ediplataform/crm/ApiEdiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ediplataform.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEdiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
